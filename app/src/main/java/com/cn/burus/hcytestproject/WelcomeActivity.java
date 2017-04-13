3a96b8311cd352c942b019ac22a9a7620d02fff4
chengyou.huang <hcy1018@126.com>
2017-04-13 15:02:48 +0800

    Initial commit  myProject

---
+++ app/src/main/java/com/cn/burus/hcytestproject/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.cn.burus.hcytestproject;
+
+import android.os.Bundle;
+
+import com.cn.burus.hcytestproject.base.BaseActivity;
+
+public class WelcomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
---
+++ app/src/main/java/com/cn/burus/hcytestproject/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.cn.burus.hcytestproject.activities;
+
+import android.os.Bundle;
+
+import com.cn.burus.hcytestproject.R;
+import com.cn.burus.hcytestproject.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
