b61e3b7fbc45c1a90e25b4790d159d940f0c50d7
hcywff <hcy1018@126.com>
2017-04-13 22:18:47 +0800

    add   activity  jump logic

--- app/src/main/java/com/cn/burus/hcytestproject/WelcomeActivity.java
+++ app/src/main/java/com/cn/burus/hcytestproject/WelcomeActivity.java
@@ -1,18 +1,33 @@
 package com.cn.burus.hcytestproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.cn.burus.hcytestproject.activities.LoginActivity;
 import com.cn.burus.hcytestproject.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class WelcomeActivity extends BaseActivity {
 
+
+    @BindView(R.id.img_welcome)
+    ImageView mImgWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
 
     }
 
 
+    @OnClick(R.id.img_welcome)
+    public void onViewClicked() {
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+    }
 }
