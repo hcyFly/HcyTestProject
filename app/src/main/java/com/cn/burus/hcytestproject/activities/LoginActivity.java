a8c1d3c03308bc03ee011128f7214a27ca7d9d1a
chengyou.huang <hcy1018@126.com>
2017-04-14 11:04:21 +0800

    loginActivity  changs  commit

--- app/src/main/java/com/cn/burus/hcytestproject/activities/LoginActivity.java
+++ app/src/main/java/com/cn/burus/hcytestproject/activities/LoginActivity.java
@@ -1,16 +1,46 @@
 package com.cn.burus.hcytestproject.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import com.cn.burus.hcytestproject.R;
 import com.cn.burus.hcytestproject.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LoginActivity extends BaseActivity {
+
+    @BindView(R.id.et_name)
+    EditText mEtName;
+    @BindView(R.id.et_password)
+    EditText mEtPassword;
+    @BindView(R.id.but_commit)
+    Button mButCommit;
+    @BindView(R.id.rl_rootview)
+    RelativeLayout mRlRootview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
+    }
+
+    @OnClick({R.id.et_name, R.id.et_password, R.id.but_commit})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.et_name:
+                break;
+            case R.id.et_password:
+                break;
+            case R.id.but_commit:
+                break;
+        }
     }
 }
